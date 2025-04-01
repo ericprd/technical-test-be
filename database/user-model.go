c40c706b6bd1507e00bbcd12d53d8436c6b10521
0xCA7 <ericprd_@outlook.com>
2025-04-01 21:23:43 +0700

    fix: duplicated email when it's optional

--- database/user-model.go
+++ database/user-model.go
@@ -6,7 +6,7 @@ type User struct {
 	ID        string    `gorm:"primaryKey;not null"`
 	FullName  string    `gorm:"size:100"`
 	Username  string    `gorm:"unique;not null"`
-	Email     string    `gorm:"unique;default=null"`
+	Email     *string   `gorm:"unique;default:null"`
 	Password  string    `gorm:"not null"`
 	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP"`
 	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP"`
--- internal/domain/user/register.go
+++ internal/domain/user/register.go
@@ -1,9 +1,9 @@
 package userdomain
 
 type Request struct {
-	ID       string `json:"id"`
-	Username string `json:"username" validate:"required"`
-	Password string `json:"password" validate:"required"`
-	Email    string `json:"email"`
-	FullName string `json:"fullname" validate:"required"`
+	ID       string  `json:"id"`
+	Username string  `json:"username" validate:"required"`
+	Password string  `json:"password" validate:"required"`
+	Email    *string `json:"email"`
+	FullName string  `json:"fullname" validate:"required"`
 }
